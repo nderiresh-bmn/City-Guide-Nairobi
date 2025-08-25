0beb0852facf4d3d09cb2344bc8468de7ff1ccd3
nderiresh-bmn <nderiresh@gmail.com>
2025-08-24 23:52:18 -0700

    Create spots.js

---
+++ data/spots.js
@@ -0,0 +1,33 @@
+// Tourist, restaurant, nightlife, rental spots in Nairobi
+const spots = [
+  {
+    name: "Nairobi National Park",
+    type: "tourist",
+    hours: "6:00 AM - 6:00 PM",
+    coords: [-1.3733, 36.8580]
+  },
+  {
+    name: "Giraffe Centre",
+    type: "tourist",
+    hours: "9:00 AM - 5:00 PM",
+    coords: [-1.4216, 36.7719]
+  },
+  {
+    name: "Carnivore Restaurant",
+    type: "restaurant",
+    hours: "12:00 PM - 11:00 PM",
+    coords: [-1.3325, 36.8190]
+  },
+  {
+    name: "Alchemist Bar",
+    type: "nightlife",
+    hours: "4:00 PM - 2:00 AM",
+    coords: [-1.2644, 36.7990]
+  },
+  {
+    name: "Airbnb – Kilimani Apartment",
+    type: "rental",
+    hours: "Check-in 2 PM",
+    coords: [-1.2921, 36.7836]
+  }
+];
